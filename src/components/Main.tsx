a12874a908e4f09e15fb402c7c9b63b365f1f210
fkdkskdfjfk <yljw225@naver.com>
2024-07-01 23:03:34 +0900

    정상 요청

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -32,58 +32,59 @@ interface StockResponse {
 
 function Main() {
   const [token, setToken] = useState<string>("");
-  // const [stock, setStock] = useState<any>("");
+  const [stock, setStock] = useState<any>("");
 
   useEffect(() => {
+    // 토큰은 1분당 1회 발급됨
     // proxy에 주소를 설정했기 때문에 
     // axios.post<TokenResponse>("https://openapivts.koreainvestment.com:29443/oauth2/tokenP",{
-    console.log(`aa`,process.env.REACT_APP_APP_KEY);
-
     axios.post<TokenResponse>("/oauth2/tokenP",{
       "grant_type": "client_credentials",
       "appkey":process.env.REACT_APP_APP_KEY,
       "appsecret":process.env.REACT_APP_APP_SECRET_KEY
     })
     .then((response)=>{
-      setToken(response.data.access_token);
-      console.log(response.data.access_token);
-      
+      setToken(response.data.access_token);      
     })
     .catch((error)=>{
       console.log(error);      
     })
 
   }, []);
-  console.log(process.env.REACT_APP_APP_KEY);
   
   // 주식현재가 일자별 api 요청
-  // axios.get<StockResponse>("/uapi/domestic-stock/v1/quotations/inquire-daily-price",{
-  //   headers: {
-  //     "authorization": `Bearer + ${token}`,
-  //     "appkey":process.env.REACT_APP_APP_KEY,
-  //     "appsecret":process.env.REACT_APP_APP_SECRET_KEY,
-  //     "tr_id": "FHKST01010400"
-  //   },
-  //   params: {
-  //     "FID_COND_MRKT_DIV_CODE": "J",  // FID 조건 시장 분류 코드(J : 주식, ETF, ETN)
-  //     "FID_INPUT_ISCD": "00700",     // FID 입력 종목코드 종목번호 (6자리) ETN의 경우, Q로 시작 (EX. Q500001)
-  //     "FID_PERIOD_DIV_CODE": "D", // FID 기간 분류 코드
-  //     "FID_ORG_ADJ_PRC": "1" // FID 수정주가 원주가 가격
-  //   }
-  // })
-  // .then((response)=>{
-  //   setStock(response.data.output)
-  //   console.log(response.data);
+  useEffect(() => {
+    console.log(`token: ${token}`);
     
-  // })
-  // .catch((error)=>{
-  //   console.log(error);      
-  // })
+    axios.get<StockResponse>("/uapi/domestic-stock/v1/quotations/inquire-daily-price",{
+      headers: {
+        "authorization": `Bearer ${token}`,
+        "appkey":process.env.REACT_APP_APP_KEY,
+        "appsecret":process.env.REACT_APP_APP_SECRET_KEY,
+        "tr_id": "FHKST01010400"
+      },
+      params: {
+        "FID_COND_MRKT_DIV_CODE": "J",  // FID 조건 시장 분류 코드(J : 주식, ETF, ETN)
+        "FID_INPUT_ISCD": "00700",     // FID 입력 종목코드 종목번호 (6자리) ETN의 경우, Q로 시작 (EX. Q500001)
+        "FID_PERIOD_DIV_CODE": "D", // FID 기간 분류 코드
+        "FID_ORG_ADJ_PRC": "1" // FID 수정주가 원주가 가격
+      }
+    })
+    .then((response)=>{
+      setStock(response.data.output)
+      console.log(response.data);
+      
+    })
+    .catch((error)=>{
+      console.log(error);      
+    })
+  }, [token]);
 
   return (
     <div>
       <p>Token: {token}</p>
-      
+      {/* <p>Stock Data: {JSON.stringify(stock)}</p> */}
+      <p>Stock Data: {stock}</p>
     </div>
   );
 }
